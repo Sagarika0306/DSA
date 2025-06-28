8d0317c07b3b65754dc817e0318360545c5997af
Sagarika0306 <sagarikaroychowdhury454@gmail.com>
2025-06-28 14:06:09 +0530

    Add files via upload

---
+++ Bubble_sort.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void Bsort(int a[],int n)
+{
+    int i,j,t;
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-1-i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                t=a[j];
+                a[j]=a[j+1];
+                a[j+1]=t;
+            }
+        }
+    }
+}
+int main()
+{
+    int a[30],i,n;
+    printf("Enter the number of elements:");
+    scanf("%d",&n);
+    printf("Enter %d element:",n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    Bsort(a,n);
+    printf("After sorting:");
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
